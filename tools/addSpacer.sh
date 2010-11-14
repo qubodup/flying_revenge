d6023d5b6325d5b200b64fc3c44af155e9f0b987
Iwan Gabovitch <qubodup@gmail.com>
2010-11-14 23:36:03 +0100

    fixed addSpacer for non-full-height sprites

--- tools/addSpacer.sh
+++ tools/addSpacer.sh
@@ -3,21 +3,19 @@
 if [ -z $2 ] ; then
 	echo -e "Give following parameters: FILE WIDTHxHEIGHT. Example:\n ./addSpacer.sh spritesheet.png 16x24"
 else
-	WIDTH=`echo $2 | sed "s/x.*//"`
-	HEIGHT=`echo $2 | sed "s/.*x//"`
 
 	# temp file names
 	TEMPNAME="temp"
 
 	# split
-	convert "$1" -crop $WIDTHx$HEIGHT "$TEMPNAME.png"
+	convert "$1" -crop $2 "$TEMPNAME.png"
 
 	# add border/frame/extent
-	for i in $TEMPNAME*; do convert "$i" -background LimeGreen -compose Copy -gravity west -splice 1x0 "$i"; done
+	for i in $TEMPNAME*; do convert "$i" -splice 1x0 "$i"; done
 
 	# montage
 	files=$(ls $TEMPNAME*.png | sort -t '-' -n -k 2 | tr '\n' ' ')
-	montage $files -tile x1 -background none -geometry +0+0 "${1%.png}-padding.png"
+	montage $files -tile x1 -background none -geometry $WIDTHx$HEIGHT+0+0 "${1%.png}-padding.png"
 
 	# cleanup
 	rm "$TEMPNAME"*
