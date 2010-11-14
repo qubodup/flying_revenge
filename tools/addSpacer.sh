a0022c8caa1324d3aaee7e49caaad69e08bc9af2
Iwan Gabovitch <qubodup@gmail.com>
2010-11-14 18:39:32 +0100

    added font and tools

---
+++ tools/addSpacer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# temp file names
+TEMPNAME="temp"
+# split
+convert "$1" -crop 16x16 "$TEMPNAME.png"
+# add border/frame/extent
+for i in $TEMPNAME*; do convert "$i" -bordercolor LimeGreen -compose Copy -border 1x0x0x0 "$i"; done
+# montage
+files=$(ls $TEMPNAME*.png | sort -t '-' -n -k 2 | tr '\n' ' ')
+montage $files -tile x1 -background none -geometry +0+0 "${1%.png}-padding.png"
+# cleanup
+rm "$TEMPNAME"*
