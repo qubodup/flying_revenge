81ffb80a013dcad80ad5f845020d6429734cf509
Iwan Gabovitch <qubodup@gmail.com>
2010-11-14 18:41:28 +0100

    release script comment

--- tools/makerelease.sh
+++ tools/makerelease.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# needs to be executed from project root
 zip flying_revenge-kill_the_humans-$1.zip *txt *gif *png *ogg *lua
 mv flying_revenge-kill_the_humans-$1.zip flying_revenge-kill_the_humans-$1.love
